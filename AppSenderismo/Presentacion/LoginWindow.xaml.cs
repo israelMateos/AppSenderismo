25cdc36d865a1ea81416e204b45fa69cc41f442c
israelMateos <israel4720@gmail.com>
2022-12-14 04:37:56 +0100

    Añadida nueva ventana (copia) y lógica para cambiar

---
+++ AppSenderismo/Presentacion/LoginWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppSenderismo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private readonly string _usuario = "admin";
+        private readonly string _pass = "admin";
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (TxtUsuario.Text.Equals(_usuario) && TxtPass.Password.Equals(_pass))
+            {
+                MainWindow mainWindow = new();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                TxtIncorrecto.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
--- AppSenderismo/Presentacion/MainWindow.xaml.cs
+++ AppSenderismo/Presentacion/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace AppSenderismo
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
