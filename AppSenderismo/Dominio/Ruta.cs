18a904007f9e07a169dbd6444c2f1bc80b30a54c
Israel Mateos Aparicio <71361287+israelMateos@users.noreply.github.com>
2023-01-16 19:20:48 +0100

    Añadidas Ruta y RutaDAO

---
+++ AppSenderismo/Dominio/Ruta.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppSenderismo.Persistencia;
+
+namespace AppSenderismo.Dominio
+{
+    public class Ruta
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Provincias { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Origen { get; set; }
+        public string Destino { get; set; }
+        public string Dificultad { get; set; }
+        public int DuracionEstimada { get; set; }
+        public string FormaAcceso { get; set; }
+        public string FormaSalida { get; set; }
+        public string MaterialNecesario { get; set; }
+        public bool ComidaEnRuta { get; set; }
+        public readonly RutaDAO Dao;
+
+        public Ruta()
+        {
+            Dao = new RutaDAO();
+        }
+
+        public Ruta(string nombre)
+        {
+            Nombre = nombre;
+            Dao = new RutaDAO();
+        }
+
+        public Ruta(int id, string nombre, string provincias, DateTime fecha,
+            string origen, string destino, string dificultad, int duracionEstimada,
+            string formaAcceso, string formaSalida, string materialNecesario,
+            bool comidaEnRuta)
+        {
+            Id = id;
+            Nombre = nombre;
+            Provincias = provincias;
+            Fecha = fecha;
+            Origen = origen;
+            Destino = destino;
+            Dificultad = dificultad;
+            DuracionEstimada = duracionEstimada;
+            FormaAcceso = formaAcceso;
+            FormaSalida = formaSalida;
+            MaterialNecesario = materialNecesario;
+            ComidaEnRuta = comidaEnRuta;
+            Dao = new RutaDAO();
+        }
+    }
+}
