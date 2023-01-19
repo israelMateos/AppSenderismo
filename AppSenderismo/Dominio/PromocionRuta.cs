ff54f92254ea110a5b88b7cd33cf4931092d0c4d
Israel Mateos Aparicio <71361287+israelMateos@users.noreply.github.com>
2023-01-19 01:07:26 +0100

    Añadidas PromocionRuta y su DAO

---
+++ AppSenderismo/Dominio/PromocionRuta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppSenderismo.Persistencia;
+
+namespace AppSenderismo.Dominio
+{
+    public class PromocionRuta
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+        public string Descripcion { get; set; }
+        public readonly PromocionRutaDAO Dao;
+
+        public PromocionRuta()
+        {
+            Dao = new PromocionRutaDAO();
+        }
+
+        public PromocionRuta(string nombre)
+        {
+            Nombre = nombre;
+            Dao = new PromocionRutaDAO();
+        }
+
+        public PromocionRuta(string nombre, string tipo, string descripcion)
+        {
+            Nombre = nombre;
+            Tipo = tipo;
+            Descripcion = descripcion;
+            Dao = new PromocionRutaDAO();
+        }
+    }
+}
---
+++ AppSenderismo/Persistencia/PromocionRutaDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppSenderismo.Dominio;
+
+namespace AppSenderismo.Persistencia
+{
+    public class PromocionRutaDAO
+    {
+        public readonly List<PromocionRuta> PromocionesRutas;
+
+        public PromocionRutaDAO()
+        {
+            PromocionesRutas = new List<PromocionRuta>();
+        }
+    }
+}
